16741dc14bc031e9aa5b0510166abf1f3198a08c
Samuel Nygaard <samuel@ambolt.io>
2022-05-20 13:30:48 +0200

    work

--- agent.py
+++ agent.py
@@ -1,15 +1,42 @@
 from math import gamma
 import random
+from player import PlayerAB
 
 class Agent:
-    board: List[List[str]]
-    player_symbol: str
+    board: None
+    player_symbol: None
 
-    def __init__(self, game) -> None:
-        self.board = game.board
-        self.player_symbol = game.player_symbol
+    def __init__(self, board, player_symbol) -> None:
+        self.board = board
+        self.player_symbol = player_symbol
+
+    def get_symbol(self, i):
+        return 1 if i == self.player_symbol else 0
+
+    def transform_board(self):
+        new_board = self.board
+        for i in range(0, 6):
+            for j in range(0, 7):
+                s = self.board[i][j]
+                if s == '-':
+                    new_board[i][j] = None
+                else:
+                    new_board[i][j] = self.get_symbol(s)
+        return new_board
 
     def next_move(self):
+        board = self.transform_board()
         board_width = len(self.board[0])
         column = random.randint(0, board_width - 1)
         return column
+
+if __name__ == '__main__':
+    board = [['-', '-', '-', 'O', '-', '-', 'O'],
+         ['-', 'X', '-', '-', 'O', 'O', '-'],
+         ['-', 'O', '-', '-', '-', '-', '-'],
+         ['O', '-', '-', 'O', 'X', '-', 'O'],
+         ['-', '-', '-', '-', '-', '-', '-'],
+         ['-', '-', 'O', '-', 'O', '-', '-']]
+    s = 'X'
+
+    print(Agent(board, s).next_move())
